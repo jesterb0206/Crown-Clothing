55fb6580b6f60ca276303bdad95c506b0177f765
jesterb0206 <jesterb@seattleu.edu>
2023-05-14 20:34:01 -0700

    Finalizing Auth Listener

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,8 @@
 import {createContext, useState, useEffect} from 'react';
-import {onAuthStateChangedListener} from '../utils/firebase/firebase';
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from '../utils/firebase/firebase';
 
 export const UserContext = createContext({
   currentUser: null,
@@ -12,8 +15,12 @@ export const UserProvider = ({children}) => {
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
-      console.log(user);
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
     });
+
     return unsubscribe;
   }, []);
 
