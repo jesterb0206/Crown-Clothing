91653feed8ca6d545dba655734f84aea4be21beb
jesterb0206 <jesterb@seattleu.edu>
2023-06-07 17:55:06 -0700

    Styled-Components Button

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
 import {
   signInWithGooglePopup,
   signInAuthUserWithEmailAndPassword,
@@ -73,7 +73,11 @@ const SignInForm = () => {
         />
         <div className='buttons-container'>
           <Button type='submit'>Sign In</Button>
-          <Button buttonType='google' type='button' onClick={signInWithGoogle}>
+          <Button
+            buttonType={BUTTON_TYPE_CLASSES.google}
+            type='button'
+            onClick={signInWithGoogle}
+          >
             Sign In With Google
           </Button>
         </div>
