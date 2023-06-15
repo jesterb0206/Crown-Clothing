b38c239b55b36f6f767efb69fc8be3098b67b6ea
jesterb0206 <jesterb@seattleu.edu>
2023-06-15 15:39:01 -0700

    Redux-Persist

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {render} from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 import App from './App';
-import {store} from './store/store';
+import {store, persistor} from './store/store';
 import './index.scss';
 
 const rootElement = document.getElementById('root');
@@ -11,9 +12,11 @@ const rootElement = document.getElementById('root');
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   rootElement
