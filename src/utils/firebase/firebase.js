78fd3ee73842449a53cb840980ae2ade3fa92541
jesterb0206 <jesterb@seattleu.edu>
2023-06-15 21:42:03 -0700

    Redux-Saga: Check User Session Saga Pt. 2

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -98,7 +98,7 @@ export const createUserDocumentFromAuth = async (
     }
   }
 
-  return userDocRef;
+  return userSnapshot;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
