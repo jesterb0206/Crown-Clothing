e2f92abeb44428c9da96413969c4b9346683cf1c
jesterb0206 <jesterb@seattleu.edu>
2023-06-19 14:08:01 -0700

    Using Redux Toolkit Action Creators

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import {setCurrentUser} from './store/user/user.action';
+import {setCurrentUser} from './store/user/user.reducer';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -20,6 +20,7 @@ const App = () => {
       if (user) {
         createUserDocumentFromAuth(user);
       }
+      console.log(setCurrentUser(user));
       dispatch(setCurrentUser(user));
     });
 
