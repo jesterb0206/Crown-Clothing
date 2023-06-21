48a2e164e7ca38f447d0952c6350e22ddcfdf9a0
jesterb0206 <jesterb@seattleu.edu>
2023-06-20 19:27:09 -0700

    Finishing Stripe Payment

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -12,11 +12,42 @@ const PaymentForm = () => {
     if (!stripe || !elements) {
       return;
     }
+
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({amount: 10000}),
+    }).then((res) => res.json());
+
+    const {
+      paymentIntent: {client_secret},
+    } = response;
+
+    console.log(client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Bradley Jester',
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Payment successful!');
+      }
+    }
   };
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay now </Button>
