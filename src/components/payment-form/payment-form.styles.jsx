f8ee2257ace30ae826f3ca9c1c85352661d9f06a
jesterb0206 <jesterb@seattleu.edu>
2023-06-20 18:41:03 -0700

    Setting Up Our PaymentForm

---
+++ src/components/payment-form/payment-form.styles.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const PaymentFormContainer = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  justify-content: center;
+`;
+
+export const FormContainer = styled.form`
+  height: 100px;
+  min-width: 500px;
+`;
