80d732b8b0824452ed6bf330fee015b7013a9119
jesterb0206 <jesterb@seattleu.edu>
2023-06-21 21:24:01 -0700

    Typing Redux Cart Files Pt. 2

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,7 +3,7 @@ import {
   selectCartCount,
   selectIsCartOpen,
 } from '../../store/cart/cart.selector';
-import {setIsCartOpen} from '../../store/cart/cart.action.js';
+import {setIsCartOpen} from '../../store/cart/cart.action';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import {CartIconContainer, ItemCount} from './cart-icon.styles';
 
