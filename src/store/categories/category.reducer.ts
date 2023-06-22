86e24ad4ac40caa7200e4f170ddcba92843e56b0
jesterb0206 <jesterb@seattleu.edu>
2023-06-21 20:11:01 -0700

    Intersections & Return Types

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -16,7 +16,7 @@ export const CATEGORIES_INITIAL_STATE: CategoriesState = {
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
   action = {} as CategoryAction
-) => {
+): CategoriesState => {
   switch (action.type) {
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
       return {...state, isLoading: true};
