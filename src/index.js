5001b1f5ce59e36337bfd5f9a89825984384d182
jesterb0206 <jesterb@seattleu.edu>
2023-06-29 10:43:01 -0700

    PWA

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {Elements} from '@stripe/react-stripe-js';
 import App from './App';
 import {store, persistor} from './store/store';
 import {stripePromise} from './utils/stripe/stripe.utils';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.scss';
 
 const rootElement = document.getElementById('root');
@@ -25,3 +26,5 @@ render(
   </React.StrictMode>,
   rootElement
 );
+
+serviceWorkerRegistration.register();
