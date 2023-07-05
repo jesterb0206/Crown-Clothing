abe1ea4a01fed11a54a21e8413afc60674aad9a0
jesterb0206 <jesterb@seattleu.edu>
2023-07-05 13:21:04 -0700

    Render with Providers and Cart Icon Tests

---
+++ src/components/cart-icon/__tests__/cart-icon.test.js
@@ -0,0 +1,22 @@
+import {screen} from '@testing-library/react';
+import CartIcon from '../cart-icon.component';
+import {renderWithProviders} from '../../../utils/test-utils';
+
+describe('Cart Icon Tests', () => {
+  test('Uses Preloaded State to Render', () => {
+    const initialCartItems = [
+      {id: 1, name: 'Item A', imageUrl: 'Test', price: 10, quantity: 1},
+    ];
+
+    renderWithProviders(<CartIcon />, {
+      preloadedState: {
+        cart: {
+          cartItems: initialCartItems,
+        },
+      },
+    });
+
+    const CartLogo = screen.getByText('1');
+    expect(CartLogo).toHaveTextContent('1');
+  });
+});
