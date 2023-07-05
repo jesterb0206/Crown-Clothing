8110a82e52a624f6dc9acc16cd011a1188e2fed9
jesterb0206 <jesterb@seattleu.edu>
2023-07-05 13:02:02 -0700

    Button Tests - Part 2

--- src/components/button/__tests__/button.component.test.js
+++ src/components/button/__tests__/button.component.test.js
@@ -1,5 +1,5 @@
 import {render, screen} from '@testing-library/react';
-import Button from '../button.component';
+import Button, {BUTTON_TYPE_CLASSES} from '../button.component';
 
 describe('Button Tests', () => {
   test('Should Render Base Button When Nothing Is Passed', () => {
@@ -7,5 +7,26 @@ describe('Button Tests', () => {
 
     const buttonElement = screen.getByRole('button');
     expect(buttonElement).toHaveStyle('background-color" black');
+  });
+
+  test('Should Render Google Button When Passed Google Button Type', () => {
+    render(<Button buttonType={BUTTON_TYPE_CLASSES.google} />);
+
+    const googleButtonElement = screen.getByRole('button');
+    expect(googleButtonElement).toHaveStyle('background-color: #4285f4');
+  });
+
+  test('Should Render Inverted Button When Passed Inverted Button Type', () => {
+    render(<Button buttonType={BUTTON_TYPE_CLASSES.inverted} />);
+
+    const invertedButton = screen.getByRole('button');
+    expect(invertedButton).toHaveStyle('background-color: white');
+  });
+
+  test('Should Be Disabled if isLoading Is True', () => {
+    render(<Button isLoading={true} />);
+
+    const buttonElement = screen.getByRole('button');
+    expect(buttonElement).toBeDisabled();
   });
 });
