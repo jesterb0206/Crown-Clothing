3d1ce6fdf065a30afa2a79d546610fc5f89f6594
jesterb0206 <jesterb@seattleu.edu>
2023-07-22 00:15:01 -0700

    Experimenting With New Homepage Layout and Design

--- src/global.styles.js
+++ src/global.styles.js
@@ -1,18 +1,23 @@
 import {createGlobalStyle} from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    color: black;
+    text-decoration: none;
+  }
+
 	body {
 		font-family: 'Open Sans Condensed';
 		padding: 20px 40px;
+
 		@media screen and (max-width: 800px) {
 			padding: 10px;
 		}
-	}
-	a {
-		text-decoration: none;
-		color: black;
-	}
-	* {
-		box-sizing: border-box;
 	}
 `;
